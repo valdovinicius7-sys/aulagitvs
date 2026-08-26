a2f23470abecdadeed1ad466b2c4c089733b3483
Vinicius Valdo dos <vv@palmares.ifpe.edu.br>
2026-08-26 10:29:46 -0300

    aulapoo

---
+++ pooflix.py
@@ -0,0 +1,33 @@
+class Filmes:
+
+    def __init__(self, genero, autor, ano, idade):
+
+        self.tipodofilme = genero
+        self.criadordofilme = autor
+        self.datadelancamento = ano
+        self.faxaetaria = idade
+        pass
+    
+
+
+
+
+class Usuario:
+
+    def __init__ (self, senha, nome, id):
+        self.senhadousuario = senha
+        self.nomedousuario = nome
+        self.idusuario = id
+    pass
+
+
+class Series:
+    def __init__(self,direção, temporada, ano ):
+        self.direcao = direção
+        self.temporada = temporada
+        self.ano= ano
+        
+    
+
+
+

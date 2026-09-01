69d2cd370afe622b87808314d104d66ad9d23fc4
viniy <vv@discente.ifpe.edu.br>
2026-09-01 09:14:32 -0300

    atualizacao da aula

--- aula.py
+++ aula.py
@@ -1,2 +1,13 @@
 print ("ViniciusValdo")
 print ("Fim de aula")
+n1 = int(input("digite um numero"))
+n2 = int(input("digite outro numero"))
+
+soma = n1 + n2
+print ("A soma dos numeros é", soma)
+multiplicacao = n1 * n2
+print ("A multiplicação dos numeros é", multiplicacao)
+subtracao = n1 - n2
+print ("A subtração dos numeros é", subtracao)
+divisao = n1 / n2
+print ("A divisão dos numeros é", divisao)
